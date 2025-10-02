83edac248cb1d658713636a8c27449a39038c246
Shubham Kumar <raoneface@gmail.com>
2025-10-02 13:12:43 +0530

    a simple class

---
+++ chapter6/A simple class/Boxdemo.java
@@ -0,0 +1,16 @@
+class box{
+    int height;
+    int width;
+    int length;
+}
+public class Boxdemo{
+    public static void main(String[] args){
+        box a1 = new box();
+        double volume;
+        a1.height=20;
+        a1.width=30;
+        a1.length=10;
+        volume = a1.height * a1.width * a1.length;
+        System.out.println("Volume of box :" + volume);
+    }
+}
