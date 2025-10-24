e89068a11d6bfd945c27b5dc7f4e1b5f3b8058f9
Shubham Kumar <raoneface@gmail.com>
2025-10-24 22:39:41 +0530

    SimpleInheritence is done

--- chapter8/inheritence Basics/A simple example of inheritence/SimpleInheritence.java
+++ chapter8/inheritence Basics/A simple example of inheritence/SimpleInheritence.java
@@ -1,47 +1,35 @@
 class A {
     int i, j;
-
     void showij() {
-        System.out.println("i and j " + i + " " + j);
+        System.out.println("i and j : " + i + " " + j);
     }
 }
-
 class B extends A {
     int k;
-
     void showk() {
-        System.out.println("k : " + k);
+        System.out.println("K : " + k);
     }
-
     void sum() {
-        System.out.println("i+j+k: " + (i + j + k));
+        System.out.println("i+j+k : " + (i + j + k));
     }
 }
-
 public class SimpleInheritence {
     public static void main(String[] args) {
-        A superob = new A();
-        B subob = new B();
-
-        superob.i = 20;
-        superob.j = 10;
-
-        System.out.println("Content of superobj ");
-        superob.showij();
+        A Superob = new A();
+        B Childob = new B();
+        Superob.i = 10;
+        Superob.j = 20;
+        System.out.println("Contents of superOb : ");
+        Superob.showij();
         System.out.println();
-
-        subob.i = 7;
-        subob.j = 14;
-        subob.k = 15;
-
-        System.out.println("Content of subob: ");
-        subob.showij();
-        subob.showk();
-
-        System.out.println();
-
-        System.out.println("sum of i, j and k ");
-
-           subob.sum();
+        Childob.i = 20;
+        Childob.j = 30;
+        Childob.k = 40;
+        System.out.println("Content of Childob : ");
+        Childob.showij();
+        Childob.showk();
+        System.out.println("Sum of i, j and k in Childob");
+        Childob.sum();
+        System.out.println("---------------------------------");
     }
 }
