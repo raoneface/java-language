849b5f76fee0165dba9d034bbcb4766d1d9d2de3
Shubham Kumar <raoneface@gmail.com>
2025-10-25 14:00:59 +0530

    Execution of  constructer

---
+++ chapter8/when Constructors are executed/execution  of constructors/CallingCons.java
@@ -0,0 +1,23 @@
+class A {
+    A() {
+        System.out.println("Inside A's Constructor.");
+    }
+}
+
+class B extends A {
+    B() {
+        System.out.println("Inside B's constructor.");
+    }
+}
+
+class C extends B {
+    C() {
+        System.out.println("Inside C's constructor.");
+    }
+}
+
+public class CallingCons {
+    public static void main(String[] args) {
+        C c = new C();
+    }
+}
